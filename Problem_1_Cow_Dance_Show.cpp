a6d8c8851598842a0982ae2bec69065477a47c1b
snujlooo <abdildaaitan@gmail.com>
2026-08-08 14:21:25 +0500

    Solved two problems from CSES 'Sorting and Searching' and USACO 2017 January Contest, Silver Problem 1.

---
+++ Problem_1_Cow_Dance_Show.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+#define sz(v) ((int)v.size())
+#define se second
+#define fi first
+#define upb upper_bound
+#define lwb lower_bound
+#define pb push_back
+#define eb emplace_back
+#define int ll
+#define pofik continue
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+// #define get(v,n) get<n>(v)
+using namespace std;
+const int maxn = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int N = 10;
+int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+int lcm(int a, int b) {return a / gcd(a, b) * b;}
+int binpow(int a, int b) {
+    int res = 1; 
+    while (b > 0) { if (b & 1) res = (res * a) % MOD; a = (a * a) % MOD; b /= 2; }
+    return res;
+}
+void up(int &x, int y) { x = max(x, y); return; }
+void down(int &x, int y) { x = min(x, y); return; }
+void solve() {
+    int n, tmax;
+    cin >> n >> tmax;
+    vector < int > dance_time(n);
+    for (int &it: dance_time) cin >> it;
+    for (int k = 1; k < n; k++) {
+        multiset < int > times;
+        for (int i = 0; i < k; i++) {
+            times.insert(dance_time[i]);
+        }
+        auto it = times.begin();
+        for (int i = k; i < n; i++) {
+            times.insert(*it + dance_time[i]);
+            it = times.erase(it);
+        }
+        int max_time = 0;
+        for (int it: times) up(max_time, it);
+        if (max_time <= tmax) {
+            cout << k; 
+            return;
+        }
+    }
+    cout << n; 
+    return;
+}
+signed main() {
+    freopen("cowdance.in", "r", stdin);
+    freopen("cowdance.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
