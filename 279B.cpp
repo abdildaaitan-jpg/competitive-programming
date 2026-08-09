4eeb2c75d26988684219cf41432451be745bd734
snujlooo <abdildaaitan@gmail.com>
2026-08-09 15:56:57 +0500

    Solved problem from Codeforces

---
+++ 279B.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+#define sz(v) ((int)v.size())
+#define se second
+#define fi first
+#define upb upper_bound
+#define lwb lower_bound
+#define pb push_back
+#define eb emplace_back
+#define int ll
+#define pofik continue
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+// #define get(v,n) get<n>(v)
+using namespace std;
+const ll maxn = 1e18;
+const ll MOD = 1e9 + 7;
+const ll N = 10;
+int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+int lcm(int a, int b) {return a / gcd(a, b) * b;}
+int binpow(int a, int b) {
+    int res = 1; 
+    while (b > 0) { if (b & 1) res = (res * a) % MOD; a = (a * a) % MOD; b /= 2; }
+    return res;
+}
+void up(int &x, int y) { x = max(x, y); return; }
+void down(int &x, int y) { x = min(x, y); return; }
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector < int > books(n);
+    for (int &it: books) cin >> it;
+    int sum = books[0], max__= 0;
+    int l = 0;
+    for (int r = 1; r <= n; r++) {
+        while (sum > k) {
+            sum -= books[l]; 
+            l++;
+        }
+        up(max__, r - l);
+        sum += books[r];
+    }
+    cout << max__;
+    return;
+}
+signed main() {
+    // freopen("cowdance.in", "r", stdin);
+    // freopen("cowdance.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
