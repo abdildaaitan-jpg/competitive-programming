d66a6c590cac2aca76ef1985f1dd88648f4c45a4
snujlooo <abdildaaitan@gmail.com>
2026-08-10 12:25:23 +0500

    Used: Binary Search On Answer, Solved problem from CSES

---
+++ Factory_Machines.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+#define sz(v) ((int)v.size())
+#define se second
+#define fi first
+#define upb upper_bound
+#define lwb lower_bound
+#define pb push_back
+#define eb emplace_back
+#define int ll
+#define pofik continue
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+// #define get(v,n) get<n>(v)
+using namespace std;
+const ll maxn = 1e18;
+const ll MOD = 1e9 + 7;
+const ll N = 10;
+int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+int lcm(int a, int b) {return a / gcd(a, b) * b;}
+int binpow(int a, int b) {
+    int res = 1; 
+    while (b > 0) { if (b & 1) res = (res * a) % MOD; a = (a * a) % MOD; b /= 2; }
+    return res;
+}
+void up(int &x, int y) { x = max(x, y); return; }
+void down(int &x, int y) { x = min(x, y); return; }
+int n, t;
+vector < int > mach;
+bool check(int t_) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += t_ / mach[i];
+    }
+    return sum >= t;
+}
+void solve() {
+    cin >> n >> t;
+    for (int i = 0; i < n; i++) {
+        int it; cin >> it;
+        mach.pb(it);
+    }
+    sort(all(mach));
+    int l = 0, r = mach[0] * t;
+    while (l != r) {
+        int mid = (l + r) / 2;
+        if (check(mid)) {
+            r = mid;
+        } 
+        else {
+            l = mid + 1;
+        }
+    }
+    cout << l;
+    return;
+}
+signed main() {
+    // freopen("cowdance.in", "r", stdin);
+    // freopen("cowdance.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
