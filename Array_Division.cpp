379c7a3662942cc18732b2cdee998941109c840a
snujlooo <abdildaaitan@gmail.com>
2026-08-12 13:10:43 +0500

    Problem will be fixen.

---
+++ Array_Division.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+#define sz(v) ((int)v.size())
+#define se second
+#define fi first
+#define upb upper_bound
+#define lwb lower_bound
+#define pb push_back
+#define eb emplace_back
+#define int ll
+#define pofik continue
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+// #define get(v,n) get<n>(v)
+using namespace std;
+const ll maxn = 1e18;
+const ll MOD = 1e9 + 7;
+const ll N = 10;
+int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+int lcm(int a, int b) {return a / gcd(a, b) * b;}
+int binpow(int a, int b) {
+    int res = 1; 
+    while (b > 0) { if (b & 1) res = (res * a) % MOD; a = (a * a) % MOD; b /= 2; }
+    return res;
+}
+void up(int &x, int y) { x = max(x, y); return; }
+void down(int &x, int y) { x = min(x, y); return; }
+int n, k;
+vector < int > nums;
+bool check(int mx_l) {
+    int cnt = 0, sum = 0, max_sum = 0;
+    for (int it: nums) {
+        if (sum + it > mx_l) {
+            up(max_sum, sum);
+            cnt++;
+            sum = it;
+        }
+        else {
+            sum += it;
+        }
+    }
+    return max_sum == mx_l && cnt == k;
+}
+void solve() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        int it;
+        cin >> it;
+        nums.pb(it);
+    }
+    int l = *min_element(all(nums)), r = accumulate(all(nums), 0);
+    while (l != r) {
+        int mid = l + (r - l + 1) / 2;
+        if (check(mid)) {
+            l = mid;
+        }
+        else {
+            r = mid - 1;
+        }
+    }
+    return;
+}
+signed main() {
+    // freopen("cowdance.in", "r", stdin);
+    // freopen("cowdance.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
