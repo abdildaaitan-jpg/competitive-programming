dceaaf88a290b62b3162a5bc8f578d345cfe7aa6
snujlooo <abdildaaitan@gmail.com>
2026-08-14 15:20:41 +0500

    Fixen: Accepted first try; Used: Binary Search On Answer, Searching; Solved problem from Codeforces

---
+++ 1850E.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+#define sz(v) ((int)v.size())
+#define se second
+#define fi first
+#define upb upper_bound
+#define lwb lower_bound
+#define pb push_back
+#define eb emplace_back
+#define int ll
+#define pofik continue
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+// #define get(v,n) get<n>(v)
+using namespace std;
+const ll maxn = 1e18 + 7;
+const ll MOD = 1e9 + 7;
+const ll N = 10;
+int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+int lcm(int a, int b) {return a / gcd(a, b) * b;}
+int binpow(int a, int b) {
+    int res = 1; 
+    while (b > 0) { if (b & 1) res = (res * a) % MOD; a = (a * a) % MOD; b /= 2; }
+    return res;
+}
+void up(int &x, int y) { x = max(x, y); return; }
+void down(int &x, int y) { x = min(x, y); return; }
+int n, c;
+int check(vector < int > scale, int num) {
+    __int128 sum = 0;
+    for (int it: scale) {
+        __int128 side = it + num * 2;
+        sum += side * side;
+        if (sum > c) return maxn;
+    }
+    return sum;
+}
+void solve() {
+    cin >> n >> c;
+    vector < int > scale(n);
+    for (int &it: scale) cin >> it;
+    int l = 1, r = c;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        int num = check(scale, mid);
+        if (num > c) {
+            r = mid - 1;
+        }
+        else if (num < c) {
+            l = mid + 1;
+        }
+        else {
+            cout << mid << '\n';
+            return;
+        }
+    }
+    return;
+}
+signed main() {
+    // freopen("cowdance.in", "r", stdin);
+    // freopen("cowdance.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
