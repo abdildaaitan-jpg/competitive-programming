e26f8fac6f80ad7b05f04335ed5f15e7d6d3b699
snujlooo <abdildaaitan@gmail.com>
2026-08-25 22:24:48 +0500

    Simple Tutos #2

---
+++ S1mpleN2.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+#define sz(v) ((int)v.size())
+#define se second
+#define fi first
+#define upb upper_bound
+#define lwb lower_bound
+#define pb push_back
+#define eb emplace_back
+#define int ll
+#define pofik continue
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+// #define get(v,n) get<n>(v)
+using namespace std;
+const ll maxn = 1e18 + 7;
+const ll MOD = 1e9 + 7;
+const ll N = 10;
+int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+int lcm(int a, int b) {return a / gcd(a, b) * b;}
+int binpow(int a, int b) {
+    int res = 1; 
+    while (b > 0) { if (b & 1) res = (res * a) % MOD; a = (a * a) % MOD; b /= 2; }
+    return res;
+}
+void up(int &x, int y) { x = max(x, y); return; }
+void down(int &x, int y) { x = min(x, y); return; }
+void solve() {
+    // C++20 жаңа тәсілі
+    vector<int> vec = {1, 5, 2, 5, 3};
+    erase(vec, 5); // Барлық 5 сандарын өшіреді
+    // Нәтиже: {1, 2, 3}
+    erase_if(vec, [](int x) { return x % 2 == 0; }); // Барлық жұп сандарды өшіреді
+    for (int it: vec) cout << it << ' ';
+    return;
+}
+signed main() {
+    // freopen("cowdance.in", "r", stdin);
+    // freopen("cowdance.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
