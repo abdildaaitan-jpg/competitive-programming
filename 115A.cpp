94b3a12036b7d88c69f9b47b1040db3305c9df8a
abdildaaitan-jpg <abdildaaitan@gmail.com>
2026-09-01 06:28:22 +0500

    WA on test 10

---
+++ 115A.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+#define sz(v) ((int)v.size())
+#define se second
+#define fi first
+#define upb upper_bound
+#define lwb lower_bound
+#define pb push_back
+#define eb emplace_back
+#define int ll
+#define pofik continue
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+// #define get(v,n) get<n>(v)
+using namespace std;
+const ll maxn = 1e18 + 7;
+const ll MOD = 1e9 + 7;
+const ll N = 10;
+int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+int lcm(int a, int b) {return a / gcd(a, b) * b;}
+int binpow(int a, int b) {
+    int res = 1; 
+    while (b > 0) { if (b & 1) res = (res * a) % MOD; a = (a * a) % MOD; b /= 2; }
+    return res;
+}
+void up(int &x, int y) { x = max(x, y); return; }
+void down(int &x, int y) { x = min(x, y); return; }
+
+int n;
+vector < vector < int >> g;
+vector < int > boss;
+vector < bool > used;
+queue < int > q;
+vector < int > team;
+int id = 1, max_ans = 0;
+
+void bfs(int st) {
+    used[st] = 1;
+    q.push(st);
+    team[st] = 1;
+
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        id = team[v] + 1;
+
+        for (int to: g[v]) {
+            if (!used[to]) {
+                team[to] = id;
+                q.push(to);
+                used[to] = 1;
+            }
+        }
+    }
+}
+
+void solve() {
+    cin >> n;
+    g.resize(n + 1); used.resize(n + 1, 0); boss.resize(n + 1); team.resize(n + 1, -1);
+    for (int i = 1; i <= n; i++) {
+        cin >> boss[i];
+        if (boss[i] == -1) pofik;
+        g[i].pb(boss[i]);
+        g[boss[i]].pb(i);
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!used[i]) {
+            bfs(i);
+        }
+    }
+    cout << *max_element(all(team));
+    return;
+}
+signed main() {
+    // freopen("cowdance.in", "r", stdin);
+    // freopen("cowdance.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
