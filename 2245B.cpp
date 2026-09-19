02d01784ee97953d343276d01d4454af6b46bbab
abdildaaitan-jpg <abdildaaitan@gmail.com>
2026-09-19 21:50:43 +0500

    Fixen: hint of AI; Used: Sorting, Greedy; Solved problem from Codeforces

---
+++ 2245B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+ 
+using ll = long long;
+#define sz(v) ((int)v.size())
+#define se second
+#define fi first
+#define upb upper_bound
+#define lwb lower_bound
+#define pb push_back
+#define eb emplace_back
+#define int ll
+#define pofik continue
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+// #define get(v,n) get<n>(v)
+using namespace std;
+const ll maxn = 1e18 + 7;
+const ll MOD = 1e9 + 7;
+const ll N = 10;
+int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+int lcm(int a, int b) {return a / gcd(a, b) * b;}
+int binpow(int a, int b) {
+    int res = 1; 
+    while (b > 0) { if (b & 1) res = (res * a) % MOD; a = (a * a) % MOD; b /= 2; }
+    return res;
+}
+void up(int &x, int y) { x = max(x, y); return; }
+void down(int &x, int y) { x = min(x, y); return; }
+ 
+int n, c;
+
+void solve() {
+    cin >> n >> c;
+    vector < int > v(n);
+    int pos = 0, val = 0;
+    for (int &it: v) {
+        cin >> it;
+        it -= c;
+        if (it > 0) pos++;
+    }
+    sort(all(v), greater < int > ());
+    int p = max((n + 1) / 2, pos);
+    for (int i = 0; i < p; i++) {
+        val += v[i];
+    }
+    cout << val << '\n';
+    return;
+}
+ 
+signed main() {
+    // freopen("cowdance.in", "r", stdin);
+    // freopen("cowdance.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
