01dc6c6697dc4faa0bbfa0747d99915f27f08d09
abdildaaitan-jpg <abdildaaitan@gmail.com>
2026-09-20 21:07:38 +0500

    Expresstions Java

---
+++ Expresstions.java
@@ -0,0 +1,10 @@
+public class Expresstions {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        double n = 2.5, m = 3.5;
+        System.out.println(a * b);
+        System.out.println(n * m);
+        System.out.println(a + b);
+        System.out.println(n + m);
+    }
+}
