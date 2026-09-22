2efccfce43a690ec79338ba53f555bc4b3ecccb2
abdildaaitan-jpg <abdildaaitan@gmail.com>
2026-09-22 17:40:44 +0500

    Fixen: hint of AI, index when getting sum; Used: Segment tree; Solved problem from CSES

---
+++ Range_Update_Queries.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+#define sz(v) ((int)v.size())
+#define se second
+#define fi first
+#define upb upper_bound
+#define lwb lower_bound
+#define pb push_back
+#define eb emplace_back
+#define int ll
+#define pofik continue
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+// #define get(v,n) get<n>(v)
+using namespace std;
+const ll maxn = 1e18 + 7;
+const ll MOD = 1e9 + 7;
+const ll N = 10;
+int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+int lcm(int a, int b) {return a / gcd(a, b) * b;}
+int binpow(int a, int b) {
+    int res = 1; 
+    while (b > 0) { if (b & 1) res = (res * a) % MOD; a = (a * a) % MOD; b /= 2; }
+    return res;
+}
+void up(int &x, int y) { x = max(x, y); return; }
+void down(int &x, int y) { x = min(x, y); return; }
+
+vector < int > t, v;
+int sum;
+
+void get(int n, int s, int e, int i) {
+    if (s > i || e < i) return;
+    if (s <= i && e >= i) sum += t[n];
+    if (s == e) return;
+    int mid = (s + e) / 2;
+    get(n << 1, s, mid, i); 
+    get(n << 1 | 1, mid + 1, e, i);
+}
+
+void build(int n, int s, int e, int l, int r, int v) {
+    if (r < s || l > e) return;
+    if (l <= s && r >= e) {
+        t[n] += v;
+        return;
+    }
+    int mid = (s + e) / 2;
+    build(n << 1, s, mid, l, r, v); 
+    build(n << 1 | 1, mid + 1, e, l, r, v);
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    t.resize(n * 4); v.resize(n + 1);
+    for (int i = 1; i <= n; i++) cin >> v[i];
+    while (m--) {
+        int i; cin >> i;
+        if (i == 1) {
+            int l, r, v;
+            cin >> l >> r >> v;
+            build(1, 1, n, l, r, v);
+        }
+        else {
+            int l; cin >> l;
+            sum = 0;
+            get(1, 1, n, l);
+            cout << v[l] + sum << '\n';
+        }
+    }
+    return;
+}
+signed main() {
+    // freopen("cowdance.in", "r", stdin);
+    // freopen("cowdance.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+    return 0;
+}
